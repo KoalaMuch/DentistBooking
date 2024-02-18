4bf04a7be029e4d62aa4dabca20468ab2751431c
Theerut <theerut.seek@gmail.com>
2024-02-18 21:51:04 +0700

    Initial setup

---
+++ BE/controllers/appointments.js
@@ -0,0 +1,131 @@
+const Appointment = require('../models/Appointment');
+const Hospital = require('../models/Hospital');
+
+//@desc Get all appointments
+//@route GET /api/v1/appointments
+//@access Public
+exports.getAppointments = async (req, res, next) => {
+    let query;
+    if (req.user.role !== 'admin') {
+        query = Appointment.find({user: req.user.id}).populate({
+            path: 'hospital',
+            select: 'name province tel'
+        });
+    } else {
+        query = Appointment.find().populate({
+            path: 'hospital',
+            select: 'name province tel'
+        });
+    }
+    try {
+        const appointments = await query;
+        res.status(200).json({success: true, count: appointments.length, data: appointments});
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({success: false, message: 'Cannot find Appointment'});
+    }
+};
+
+//@desc Get single appointment
+//@route GET /api/v1/appointments/:id
+//@access Public
+exports.getAppointment = async (req, res, next) => {
+    try {
+        const appointment = await Appointment.findById(req.params.id).populate({
+            path: 'hospital',
+            select: 'name province tel'
+        });
+        if (!appointment) {
+            return res.status(404).json({success: false, message: `Cannot find Appointment with id ${req.params.id}`});
+        }
+        res.status(200).json({success: true, data: appointment});
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({success: false, message: 'Cannot find Appointment'});
+    }
+}
+
+//@desc Add appointment
+//@route POST /api/v1/hospitals/:hospitalId/appointments
+//@access Private
+exports.addAppointment = async (req, res, next) => {
+    try {
+
+        // Add user to req.body
+        req.body.user = req.user.id;
+
+        // Check for exist appointment
+        const existedAppointments = await Appointment.find({user: req.user.id});
+        
+        // If the user is not admin, they can only create 3 appointment.
+        if(req.user.role !== 'admin' && existedAppointments.length >= 3) {
+            return res.status(400).json({success: false, message: 'The user with ID ' + req.user.id + ' has already booked 3 appointments'});
+        }
+
+        req.body.hospital = req.params.hospitalId;
+        const hospital = await Hospital.findById(req.params.hospitalId);
+        if (!hospital) {
+            return res.status(404).json({success: false, message: `No hospital with the id of ${req.params.hospitalId}`});
+        }
+        const appointment = await Appointment.create(req.body);
+        res.status(200).json({success: true, data: appointment});
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({success: false, message: 'Cannot create Appointment'});
+    }
+}
+
+//@desc Update appointment
+//@route PUT /api/v1/appointments/:id
+//@access Private
+
+exports.updateAppointment = async (req, res, next) => {
+    try {
+        let appointment = await Appointment.findById(req.params.id);
+        if (!appointment) {
+            return res.status(404).json({success: false, message: `Cannot find Appointment with id ${req.params.id}`});
+        }
+
+        // Make sure user is appointment owner
+        if (appointment.user.toString() !== req.user.id && req.user.role !== 'admin') {
+            return res.status(401).json({success: false, message: `User ${req.params.id} is not authorized to update this appointment`});
+        }
+
+        appointment = await Appointment.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        res.status(200).json({success: true, data: appointment});
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({success: false, message: 'Cannot update Appointment'});
+    }
+}
+
+//@desc Delete appointment
+//@route DELETE /api/v1/appointments/:id
+//@access Private
+exports.deleteAppointment = async (req, res, next) => {
+    try {
+        const appointment = await Appointment.findById(req.params.id);
+        if (!appointment) {
+            return res.status(404).json({success: false, message: `Cannot find Appointment with id ${req.params.id}`});
+        }
+
+        // Make sure user is appointment owner
+        if (appointment.user.toString() !== req.user.id && req.user.role !== 'admin') {
+            return res.status(401).json({success: false, message: `User ${req.params.id} is not authorized to delete this appointment`});
+        }
+
+        await appointment.deleteOne();
+        res.status(200).json({success: true, message: 'Appointment deleted'});
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({success: false, message: 'Cannot delete Appointment'});
+    }
+}
---
+++ BE/server.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const { xss } = require("express-xss-sanitizer");
+const rateLimit = require("express-rate-limit");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+const app = express();
+app.use(cors());
+
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100,
+});
+app.use(limiter);
+
+// var allowCrossDomain = function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// };
+// app.use(allowCrossDomain);
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Libraly API",
+      version: "1.0.0",
+      description: "A simple Express VacQ API",
+    },
+    servers: [
+      {
+        url: "http://localhost:20000/api/v1",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+const hospitals = require("./routes/hospitals");
+
+// Mount routers
+app.use("/api/v1/hospitals", hospitals);
+
+const auth = require("./routes/auth");
+
+// Mount routers
+app.use("/api/v1/auth", auth);
+
+const appointments = require("./routes/appointments");
+
+// Mount routers
+app.use("/api/v1/appointments", appointments);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+// Handle unhandled promise rejections
+// This is a global handler for unhandled promise rejections.
+// It should be at the bottom of server.js.
+// It is a best practice to always add this.
+// It will prevent the server from crashing if there is an unhandled promise rejection.
+// It will also log the error to the console.
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
