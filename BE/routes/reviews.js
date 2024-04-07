96ff432ef8005d8035b681d1218d129e048a81bf
ratnart <nartgg35665@gmail.com>
2024-04-07 20:51:28 +0700

    Delete review

--- BE/routes/reviews.js
+++ BE/routes/reviews.js
@@ -174,27 +174,3 @@
  *       404:
  *         description: The review was not found
  */
-
-const express = require("express");
-
-const {
-  getReviews,
-  createReview,
-  updateReview,
-  deleteReview,
-} = require("../controllers/reviews");
-
-const { protect, authorize } = require("../middleware/auth");
-
-const router = express.Router();
-
-router.route("/").post(protect, authorize("user"), createReview);
-
-router.route("/:dentistId").get(getReviews);
-
-router
-  .route("/:id")
-  .put(protect, authorize("user", "admin"), updateReview)
-  .delete(protect, authorize("user", "admin"), deleteReview);
-
-module.exports = router;
