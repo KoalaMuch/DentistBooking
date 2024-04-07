016ee16c67a254b63b8e7720faa5b7dfae3a875d
Theerut <theerut.seek@gmail.com>
2024-04-07 12:28:40 +0700

    Update model schema

--- BE/models/Review.js
+++ BE/models/Review.js
@@ -1,47 +1,25 @@
 const mongoose = require("mongoose");
 
-const ReviewSchema = new mongoose.Schema(
-  {
-    user: {
-      type: mongoose.Schema.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    dentist: {
-      type: mongoose.Schema.ObjectId,
-      ref: "Dentist",
-      required: true,
-    },
-    rating: {
-      type: Number,
-      require: [true, "Please add a rating"],
-      min: 0,
-      max: 5,
-    },
+const ReviewSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
   },
-  {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
-  }
-);
-
-// Cascade delete appointments when a hospital is deleted
-HospitalSchema.pre(
-  "deleteOne",
-  { document: true, query: false },
-  async function (next) {
-    console.log(`Appointments being removed from dentist ${this._id}`);
-    await this.model("Appointment").deleteMany({ hospital: this._id });
-    next();
-  }
-);
-
-// Reverse populate with virtuals
-DentistSchema.virtual("appointments", {
-  ref: "Appointment",
-  localField: "_id",
-  foreignField: "dentist",
-  justOne: false,
+  dentist: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Dentist",
+    required: true,
+  },
+  rating: {
+    type: Number,
+    require: [true, "Please add a rating"],
+    min: 0,
+    max: 5,
+  },
+  comment: {
+    type: String,
+    required: [true, "Please add a comment"],
+  },
 });
-
-module.exports = mongoose.model("Dentist", DentistSchema);
+module.exports = mongoose.model("Review", DentistSchema);
