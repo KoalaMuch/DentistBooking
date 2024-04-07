1824b71ab2015d0fc60aea1732aba8715994d1d2
Theerut <theerut.seek@gmail.com>
2024-04-07 14:09:13 +0700

    add review route

---
+++ BE/routes/reviews.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const {
+  getReviews,
+  createReview,
+  updateReview,
+  deleteReview,
+} = require("../controllers/reviews");
+
+const { protect, authorize } = require("../middleware/auth");
+
+const router = express.Router();
+
+router.route("/").post(protect, authorize("user"), createReview);
+
+router.route("/:dentistId").get(getReviews);
+
+router
+  .route("/:id")
+  .put(protect, authorize("user", "admin"), updateReview)
+  .delete(protect, authorize("user", "admin"), deleteReview);
